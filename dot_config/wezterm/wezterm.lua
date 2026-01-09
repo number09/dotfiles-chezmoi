4dd0cd5bcc5a15fd254206642ae407c9bcca0c95
Koichi Ogura <12413803+number09@users.noreply.github.com>
2026-01-09 21:14:28 +0900

    フォントサイズ修正

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -21,12 +21,30 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 10
+config.font_size = 14
 -- color scheme
 config.color_scheme = "iceberg-dark"
 
 -- font
 config.font = wezterm.font("Ricty Diminished")
+
+config.keys = {
+  -- パネル分割
+  { key = 'd', mods = 'CMD|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } }, -- 縦分割 (CMD+Shift+|)
+  { key = 'd', mods = 'CMD',       action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },   -- 横分割 (CMD+-)
+
+  -- パネル移動
+  { key = 'h', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'CMD',       action = wezterm.action.ActivatePaneDirection 'Right' },
+
+  -- パネルを閉じる
+  { key = 'w', mods = 'CMD',       action = wezterm.action.CloseCurrentPane { confirm = true } },
+}
+
+
+
 
 -- Finally, return the configuration to wezterm:
 return config
