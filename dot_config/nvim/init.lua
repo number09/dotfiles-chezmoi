8ad511a6e66dc4015aa9ed18dfe1500fe6b99493
Koichi Ogura <12413803+number09@users.noreply.github.com>
2026-07-10 09:36:05 +0900

    herder上でもvimヤンク時にクリップボード共有

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,8 +7,19 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
--- クリップボードに追加
+-- クリップボードに追加（OSC 52 経由でマルチプレクサ内でも動作）
 vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 
 
 require("config.lazy")
